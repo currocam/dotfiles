72d788620d8d8dc4daf0d125303f5cf3bc604656
currocam <campuzanocurro@gmail.com>
2022-07-06 18:28:18 +0200

    custom

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------------------------
 # Codely theme config
 # ------------------------------------------------------------------------------
-export CODELY_THEME_MINIMAL=false
-export CODELY_THEME_MODE="dark"
+export CODELY_THEME_MINIMAL=true
+export CODELY_THEME_MODE="ligth"
 export CODELY_THEME_PROMPT_IN_NEW_LINE=false
 export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
 
