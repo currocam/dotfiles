1c2994672cc59be3f03495d99697e9ba4e029faf
currocam <campuzanocurro@gmail.com>
2022-07-08 08:58:10 +0200

    custom aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -14,7 +14,7 @@ alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status -sb"
+alias gs="git status"
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
@@ -22,6 +22,9 @@ alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
 alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
+# Custom
+alias please='sudo $(fc -l -n -1)'
+alias bat='batcat'
 # Utils
 alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
